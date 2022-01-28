373c58e750e106fa666895c9eb3c1dff05012529
Joshua Reynolds <71860187+Joshua-Reynolds-MTU@users.noreply.github.com>
2022-01-28 17:36:02 -0500

    Add files via upload

---
+++ NewSceneSwitcher.cs
@@ -0,0 +1,11 @@
+﻿using System.Collections; using System.Collections.Generic; using UnityEngine; using UnityEngine.UI; using UnityEngine.SceneManagement;  public class NewSceneSwitcher : MonoBehaviour {      public void Awake()     {         DontDestroyOnLoad(this);     }      public void playGame()
+    {
+        SceneManager.LoadScene("SlidesScene");
+    }      public void starter()     {         SceneManager.LoadScene(sceneBuildIndex: 5);     }      public void invokeSwitcher()
+    {
+        int index = Random.Range(1, 5);
+        SceneManager.LoadScene(index);
+    }      public void quitGame()     {         Debug.Log("Quitting game...");         Application.Quit();     }      public void tenSecondBack()
+    {
+        SceneManager.LoadScene("SlidesScene");
+    } }
---
+++ SecondBackScript.cs
@@ -0,0 +1,20 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class SecondBackScript : MonoBehaviour
+{
+
+    private NewSceneSwitcher scene1;
+
+    void Start()
+    {
+        Invoke("sceneGrabber2", 4f);
+    }
+
+    void sceneGrabber2()
+    {
+        scene1 = GameObject.Find("GameManager").GetComponent<NewSceneSwitcher>();
+        scene1.tenSecondBack();
+    }
+}
